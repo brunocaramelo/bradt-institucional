93d8e97717dc034bc1e21544593bd6b202d60910
Bruno Caramelo Souza <bruno.caramelo@ftd.com.br>
2021-05-14 17:17:31 -0300

    curoso

--- storage/framework/views/45bfb22debfdba056a28c36a62869a5a86be5b7d.php
+++ storage/framework/views/45bfb22debfdba056a28c36a62869a5a86be5b7d.php
@@ -1,8 +1,9 @@
 <nav id="xwezi-main-menu">
     <ul>
         <li><a class="cool-link" href=<?php echo e(route('home')); ?>>Home</a></li>
-        <li><a class="cool-link" href=<?php echo e(route('presential-courses')); ?>>Cursos presenciais</a></li>
-        <li><a class="cool-link" href=<?php echo e(route('online-courses')); ?>>Cursos Online</a></li>
+        <li><a class="cool-link" href=<?php echo e(url('portal/')); ?>>Blog</a></li>
+        <li><a class="cool-link" href=<?php echo e(url('portal/?page_id=6')); ?>>Cursos</a></li>
+        
         <li><a class="cool-link" href=<?php echo e(route('attendance')); ?>>Atendimentos</a></li>
         <li><a class="cool-link active" href=<?php echo e(route('contact')); ?>>Contato</a></li>
     </ul>
